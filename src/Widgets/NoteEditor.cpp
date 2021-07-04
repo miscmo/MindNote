e6cafe1c5a03375fa740cd15ba539cd5bfe6881c
LHG <754657908@qq.com>
2021-07-04 23:02:01 +0800

    增加行号

--- src/Widgets/NoteEditor.cpp
+++ src/Widgets/NoteEditor.cpp
@@ -11,6 +11,8 @@
 #include <QTextEdit>
 #include <QTreeWidgetItem>
 #include <QVBoxLayout>
+#include <QPainter>
+#include <QTextBlock>
 
 #include <QDebug>
 
@@ -31,25 +33,129 @@ NoteEditor::~NoteEditor() {
 }
 
 NoteEditor::NoteEditor(QWidget *parent)
-    : QWidget(parent) {
-
+    : QPlainTextEdit(parent) {
     initUi();
 
     setupSignal();
 }
 
 QString NoteEditor::getText() {
-    return m_pTextEdit->toPlainText();
+    return toPlainText();
 }
 
 void NoteEditor::initUi() {
-    m_pMainLayout = new QVBoxLayout(this);
-    m_pMainLayout->setContentsMargins(0, 0, 0, 0);
+    //setFontFamily("Consolas");
 
-    m_pTextEdit = new QTextEdit(this);
+    m_pLineNumberArea = new LineNumberArea(this);
 
-    m_pMainLayout->addWidget(m_pTextEdit);
+
+    updateLineNumberAreaWidth(0);
+    highlightCurrentLine();
 }
+
+int NoteEditor::lineNumberAreaWidth() {
+    int digits = 1;
+    int max = qMax(1, blockCount());
+    while (max >= 10) {
+        max /= 10;
+        ++digits;
+    }
+
+    int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+
+    return space;
+}
+
+void NoteEditor::updateLineNumberAreaWidth(int /* newBlockCount */)
+{
+    setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+}
+
+//![slotUpdateExtraAreaWidth]
+
+//![slotUpdateRequest]
+
+void NoteEditor::updateLineNumberArea(const QRect &rect, int dy)
+{
+    if (dy)
+        m_pLineNumberArea->scroll(0, dy);
+    else
+        m_pLineNumberArea->update(0, rect.y(), m_pLineNumberArea->width(), rect.height());
+
+    if (rect.contains(viewport()->rect()))
+        updateLineNumberAreaWidth(0);
+}
+
+//![slotUpdateRequest]
+
+//![resizeEvent]
+
+void NoteEditor::resizeEvent(QResizeEvent *e)
+{
+    QPlainTextEdit::resizeEvent(e);
+
+    QRect cr = contentsRect();
+    m_pLineNumberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
+}
+
+//![resizeEvent]
+
+//![cursorPositionChanged]
+
+void NoteEditor::highlightCurrentLine()
+{
+    QList<QTextEdit::ExtraSelection> extraSelections;
+
+    if (!isReadOnly()) {
+        QTextEdit::ExtraSelection selection;
+
+        QColor lineColor = QColor(Qt::yellow).lighter(160);
+
+        selection.format.setBackground(lineColor);
+        selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+        selection.cursor = textCursor();
+        selection.cursor.clearSelection();
+        extraSelections.append(selection);
+    }
+
+    setExtraSelections(extraSelections);
+}
+
+//![cursorPositionChanged]
+
+//![extraAreaPaintEvent_0]
+
+void NoteEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
+{
+    QPainter painter(m_pLineNumberArea);
+    painter.fillRect(event->rect(), Qt::lightGray);
+
+//![extraAreaPaintEvent_0]
+
+//![extraAreaPaintEvent_1]
+    QTextBlock block = firstVisibleBlock();
+    int blockNumber = block.blockNumber();
+    int top = (int) blockBoundingGeometry(block).translated(contentOffset()).top();
+    int bottom = top + (int) blockBoundingRect(block).height();
+//![extraAreaPaintEvent_1]
+
+//![extraAreaPaintEvent_2]
+    while (block.isValid() && top <= event->rect().bottom()) {
+        if (block.isVisible() && bottom >= event->rect().top()) {
+            QString number = QString::number(blockNumber + 1);
+            painter.setPen(Qt::black);
+            painter.drawText(0, top, m_pLineNumberArea->width(), fontMetrics().height(),
+                             Qt::AlignRight, number);
+        }
+
+        block = block.next();
+        top = bottom;
+        bottom = top + (int) blockBoundingRect(block).height();
+        ++blockNumber;
+    }
+}
+
+
 
 void NoteEditor::setupUi() {
 }
@@ -57,10 +163,14 @@ void NoteEditor::setupUi() {
 void NoteEditor::setupSignal() {
     connect(BufferManager::getInstance(), &BufferManager::currentBufferChanged,
             this, &NoteEditor::onCurBufferChanged);
+    connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth(int)));
+    connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLineNumberArea(QRect,int)));
+    connect(this, SIGNAL(cursorPositionChanged()), this, SLOT(highlightCurrentLine()));
 }
 
 void NoteEditor::onCurBufferChanged(Buffer *buffer) {
-    m_pTextEdit->setText(buffer->read());
+    //setText(buffer->read());
+    setPlainText(buffer->read());
 }
 
 }
